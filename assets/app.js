07dc479f45bc0d2d376e6660ccbb0c3b4a1b4de8
hive-lab <hive-lab@users.noreply.github.com>
2015-02-27 14:25:08 +0300

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,28 @@
 var hiveLabs = angular.module('hiveLabs', ['ngRoute', 'firebase'])
 .controller('postCtrl', function($scope, $routeParams, $firebase) {
+     $scope.defArt = "Internet Of Things";
+     $scope.defRels = [
+     {title: "Habrahabr", site: "Habrahabr", link: "http://habrahabr.ru"}, 
+     {title: "EasyElectronics", site: "EasyElectronics", link: "http://easyelectronics.ru"}
+     ];
      $scope.name = "postCtrl";
      $scope.params = $routeParams;
  })
+.directive('pHeader', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'post_h.html'
+ };
+.directive('pRelated', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'post_r.html'
+ };
+.directive('pToc', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'post_t.html'
+ }; 
 hiveLabs.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
